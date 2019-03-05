d302bc06f7b65ff4a04b02c06b1d4585df9a6e27
antonia-had <ah986@cornell.edu>
2019-03-05 16:02:31 -0500

    Update fish_game_mordm.py

--- examples/Fish_game/fish_game_mordm.py
+++ examples/Fish_game/fish_game_mordm.py
@@ -169,5 +169,11 @@ model.levers = [RealLever("vars", 0.0, 1.0, length = 6)]
 
 output = optimize(model, "NSGAII", 1000)
 
-SOWs = sample_lhs(model, 4000)
+SOWs = sample_lhs(model, 1000)
+policy = output.find_max("NPV")
 results = evaluate(model, update(SOWs, policy))
+classification = results.apply("'Survival' if PredatorExtinction < 1 else 'Extinction'")
+p = Prim(results, classification, include=model.uncertainties.keys(), coi="Survival")
+box = p.find_box()
+fig = box.show_tradeoff()
+
