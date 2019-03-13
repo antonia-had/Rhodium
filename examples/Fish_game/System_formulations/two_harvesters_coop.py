a892f8476b6f152797ebc8a387202f7bbcea7cbc
antonia-had <ah986@cornell.edu>
2019-03-13 13:06:08 -0400

    Update two_harvesters_coop.py

--- examples/Fish_game/System_formulations/two_harvesters_coop.py
+++ examples/Fish_game/System_formulations/two_harvesters_coop.py
@@ -2,7 +2,7 @@ import numpy as np
 import itertools
 import sys
 sys.path.append('../')
-from Harvesting_formulations.gaussian_OG import hrvSTR
+from Harvesting_formulations.two_coop import hrvSTR
 
 N = 100 # Number of realizations of environmental stochasticity
 
@@ -22,19 +22,20 @@ def fish_game(vars, # contains all C, R, W for RBF policy
 
     x = np.zeros(tSteps+1) # Create prey population array
     y = np.zeros(tSteps+1) # Create predator population array
-    z = np.zeros(tSteps+1) # Create harvest array
+    z_a = np.zeros(tSteps+1) # Create harvest array
+    z_b = np.zeros(tSteps+1)
 
     # Create array to store harvest for all realizations
-    harvest = np.zeros([N,tSteps+1])
-    # Create array to store effort for all realizations
-    effort = np.zeros([N,tSteps+1])
+    harvest_a = np.zeros([N,tSteps+1])
+    harvest_b = np.zeros([N,tSteps+1])
     # Create array to store prey for all realizations
     prey = np.zeros([N,tSteps+1])
     # Create array to store predator for all realizations
     predator = np.zeros([N,tSteps+1])
     
     # Create array to store metrics per realization
-    NPV = np.zeros(N)
+    NPV_a = np.zeros(N)
+    NPV_b = np.zeros(N)
     cons_low_harv = np.zeros(N)
     harv_1st_pc = np.zeros(N)    
     
@@ -49,32 +50,36 @@ def fish_game(vars, # contains all C, R, W for RBF policy
         # Initialize populations and values
         x[0] = prey[i,0] = K
         y[0] = predator[i,0] = 250
-        z[0] = effort[i,0] = hrvSTR([x[0]], vars, [[0, K]], [[0, 1]])
-        NPVharvest = harvest[i,0] = effort[i,0]*x[0]        
+        z_a[0], z_b[0] = hrvSTR([x[0]], vars, [[0, K]], [[0, 1]])
+        NPVharvest_a = harvest_a[i,0] = z_a[0]*x[0]   
+        NPVharvest_b = harvest_b[i,0] = z_b[0]*x[0] 
         # Go through all timesteps for prey, predator, and harvest
         for t in range(tSteps):
             if x[t] > 0 and y[t] > 0:
-                x[t+1] = (x[t] + b*x[t]*(1-x[t]/K) - (a*x[t]*y[t])/(np.power(y[t],m)+a*h*x[t]) - z[t]*x[t])* np.exp(epsilon_prey[i]) # Prey growth equation
+                x[t+1] = (x[t] + b*x[t]*(1-x[t]/K) - (a*x[t]*y[t])/(np.power(y[t],m)+a*h*x[t]) - z_a[t]*x[t] - (1-z_a[t])*z_b[t]*x[t])* np.exp(epsilon_prey[i]) # Prey growth equation
                 y[t+1] = (y[t] + c*a*x[t]*y[t]/(np.power(y[t],m)+a*h*x[t]) - d*y[t]) *np.exp(epsilon_predator[i]) # Predator growth equation
                 if t <= tSteps-1:
                     input_ranges = [[0, K]] # Prey pop. range to use for normalization
                     output_ranges = [[0, 1]] # Range to de-normalize harvest to
-                    z[t+1] = hrvSTR([x[t]], vars, input_ranges, output_ranges)
+                    z_a[t+1], z_b[t+1] = hrvSTR([x[t]], vars, input_ranges, output_ranges)
             prey[i,t+1] = x[t+1]
             predator[i,t+1] = y[t+1]
-            effort[i,t+1] = z[t+1]
-            harvest[i,t+1] = z[t+1]*x[t+1]
-            NPVharvest = NPVharvest + harvest[i,t+1]*(1+0.05)**(-(t+1))
-        NPV[i] = NPVharvest
-        low_hrv = [harvest[i,j]<prey[i,j]/20 for j in range(len(harvest[i,:]))] # Returns a list of True values when there's harvest below 5%
+            harvest_a[i,t+1] = z_a[t+1]*x[t+1]
+            harvest_b[i,t+1] = (1-z_a[t+1])*z_b[t+1]*x[t+1]
+            NPVharvest_a = NPVharvest_a + harvest_a[i,t+1]*(1+0.05)**(-(t+1))
+            NPVharvest_b = NPVharvest_b + harvest_b[i,t+1]*(1+0.05)**(-(t+1))
+        NPV_a[i] = NPVharvest_a
+        NPV_b[i] = NPVharvest_b
+        low_hrv = [harvest_a[i,j]<prey[i,j]/20 for j in range(len(harvest_a[i,:]))] # Returns a list of True values when there's harvest below 5%
         count = [ sum( 1 for _ in group ) for key, group in itertools.groupby( low_hrv ) if key ] # Counts groups of True values in a row
         if count: # Checks if theres at least one count (if not, np.max won't work on empty list)
             cons_low_harv[i] = np.max(count)  # Finds the largest number of consecutive low harvests
         else:
             cons_low_harv[i] = 0
-        harv_1st_pc[i] = np.percentile(harvest[i,:],1)
+        harv_1st_pc[i] = np.percentile(harvest_a[i,:],1)
     
-    return (np.mean(NPV), # Mean NPV for all realizations
+    return (np.mean(NPV_a), # Mean NPV for all realizations
+            np.mean(NPV_b), # Mean NPV for all realizations
             np.mean((K-prey)/K), # Mean prey deficit
             np.mean(cons_low_harv), # Mean worst case of consecutive low harvest across realizations
             np.mean(harv_1st_pc), # 5th percentile of all harvests
