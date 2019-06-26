0b1a416ae442c9abef8621cf77779bf284283f5b
Antonia Hadjimichael <ah986@cornell.edu>
2019-06-26 17:30:57 -0400

    edits to noise formulation

--- examples/Fish_game/run_rhodium_noise.py
+++ examples/Fish_game/run_rhodium_noise.py
@@ -1,6 +1,6 @@
 import sys
 sys.path.append('../')
-from System_formulations.two_harvesters_coop_noise_hrv_data import fish_game
+from System_formulations.two_harvesters_coop_noise import fish_game
 from rhodium import * 
 #from j3 import J3
 
@@ -38,15 +38,16 @@ model.uncertainties = [UniformUncertainty("a", 0.002, 0.05),
 
 model.levers = [RealLever("vars", 0.0, 1.0, length = 12)]
 
-output = optimize(model, "NSGAII", 700)
+output = optimize(model, "NSGAII", 1000)
 
-fig = parallel_coordinates(model, output, colormap="Blues", c= "NPV_a", target="top")
+fig1 = parallel_coordinates(model, output, colormap="Blues", c= "NPV_a", target="top")
 #
 #J3(output.as_dataframe(list(model.responses.keys())))
 #
-SOWs = sample_lhs(model, 1000)
-policy = output.find_max("NPV_b")
-results = evaluate(model, update(SOWs, policy))
+#SOWs = sample_lhs(model, 1000)
+#policy = output.find_max("NPV_b")
+#results = evaluate(model, update(SOWs, policy))
+#fig2 = parallel_coordinates(model, results, colormap="Blues", c= "NPV_a", target="top")
 ##
 #result = sa(model, "NPV", policy=policy, method="sobol", nsamples=1000)
 #
